3285cdc8b5d86d067eb549e1e114f3157f01cf48
Ziqing Huang <zqhuang@brandeis.edu>
2019-10-12 11:16:41 +0800

    Initial commit

---
+++ src/main/java/com/linker/gmessage/GmessageApplication.java
@@ -0,0 +1,14 @@
+package com.linker.gmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GmessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linker/gmessage/GmessageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.linker.gmessage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GmessageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
