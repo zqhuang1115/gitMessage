99e48d9958d256cc768a386cacbdf05bb8fe1f81
Ziqing Huang <zqhuang@brandeis.edu>
2019-10-12 15:52:41 +0800

    add

--- src/main/java/com/linker/gmessage/util/MapMarkdownUtils.java
+++ src/main/java/com/linker/gmessage/util/MapMarkdownUtils.java
@@ -1,8 +1,38 @@
 package com.linker.gmessage.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+
+import java.text.ParseException;
+import java.util.Map;
+
 /**
  * @author huang.ziqing
  * @date 2019/10/12
  */
 public class MapMarkdownUtils {
+    public static String markdownString(Map<String, Object> map) {
+        Gson gson = new Gson();
+        StringBuffer paramStr = new StringBuffer();
+        final String[] returnString = new String[1];
+        String action = String.valueOf(map.get("action"));
+
+        map.forEach((k, v) -> {
+            if (k.equals("data")) {
+                Map<String, Object> mapdata;
+                Map<String, Object> mapproject = null;
+                try {
+                    mapdata = gson.fromJson((new ObjectMapper()).writeValueAsString(v), map.getClass());
+                    if (mapdata.containsKey("project")) {
+                        mapproject = gson.fromJson(new ObjectMapper().writeValueAsString(mapdata.get("project")), map.getClass());
+                        paramStr.append("<font color=\"info\">【" + mapproject.get("name") + "】</font>");
+                    }
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return returnString[0];
+    }
 }
--- src/main/java/com/linker/gmessage/util/MapTextUtils.java
+++ src/main/java/com/linker/gmessage/util/MapTextUtils.java
@@ -1,8 +1,41 @@
 package com.linker.gmessage.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+
 /**
  * @author huang.ziqing
  * @date 2019/10/12
  */
 public class MapTextUtils {
+    public static  String textString(Map<String,Object> map) {
+        Gson gson = new Gson();
+        StringBuffer paramStr = new StringBuffer();
+        final String[] returnString = new String[1];
+        String action = String.valueOf(map.get("action"));
+        map.forEach((k, v) ->{
+            if(k.equals("data")){
+                Map<String, Object> mapdata ;
+                try {
+                    mapdata = gson.fromJson((new ObjectMapper()).writeValueAsString(v), map.getClass());
+                    if(mapdata.containsKey("project")){
+                        Map<String, Object> mapproject = gson.fromJson(new ObjectMapper().writeValueAsString(mapdata.get("project")), map.getClass());
+                        paramStr.append("["+mapproject.get("name")+"]");
+                    }
+
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return returnString[0];
+
+    }
 }
