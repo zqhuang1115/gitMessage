4ac057a9339b3a7f50212d81d04b5ac548a004fd
Ziqing Huang <zqhuang@brandeis.edu>
2019-10-12 13:52:40 +0800

    add gmessage

--- src/main/java/com/linker/gmessage/GmessageApplication.java
+++ src/main/java/com/linker/gmessage/GmessageApplication.java
@@ -1,17 +1,42 @@
 package com.linker.gmessage;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import utils.DefaultProfileUtil;
 
+import java.net.InetAddress;
 
+@Slf4j
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.linker.gmessage.repository","**.repository"})
 public class GmessageApplication {
 
-    String name(){
-        return "hello";
-    }
     public static void main(String[] args) {
-        SpringApplication.run(GmessageApplication.class, args);
-    }
+        SpringApplication app = new SpringApplication(GmessageApplication.class);
 
+        DefaultProfileUtil.addDefaultProfile(app);
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}\n\t" +
+                        "External: \t{}://{}:{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                env.getProperty("server.port"),
+                protocol,
+                hostAddress,
+                env.getProperty("server.port"),
+                env.getActiveProfiles());
+    }
 }
