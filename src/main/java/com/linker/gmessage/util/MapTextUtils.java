45bb3a8d33bd1897024186597fb8c79f47c2fdb2
Ziqing Huang <zqhuang@brandeis.edu>
2019-10-15 15:50:32 +0800

    test

--- src/main/java/com/linker/gmessage/util/MapTextUtils.java
+++ src/main/java/com/linker/gmessage/util/MapTextUtils.java
@@ -16,61 +16,52 @@ import java.util.Map;
  * @date 2019/10/12
  */
 public class MapTextUtils {
-    public static  String textString(Map<String,Object> map) {
+    public static  String textString(Map<String,Object> map) throws ParseException {
         Gson gson = new Gson();
         StringBuffer paramStr = new StringBuffer();
-        final String[] returnString = new String[1];
+        Map<String, Object> mappusher = null;
+        Map<String, Object> mapmessage = null;
+        Map<String, Object> maprepo = null;
+        // final String[] returnString = new String[1];
         // String action = String.valueOf(map.get("action"));
-        //String action = String.valueOf(map);
-//        System.out.println("this is k"+map.keySet());
-//        System.out.println(map);
-//        System.out.println(map.get("pusher"));
-        map.forEach((k, v) -> {
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            String k = entry.getKey();
+            Object v = entry.getValue();
             if (k.equals("pusher")) {
-                //System.out.println(name);
-                Map<String, Object> mapdata;
                 try {
-                    mapdata = gson.fromJson((new ObjectMapper()).writeValueAsString(v), map.getClass());
-                    if (mapdata.containsKey("repository")) {
-                        Map<String, Object> maprepo = gson.fromJson(new ObjectMapper().writeValueAsString(mapdata.get("repository")), map.getClass());
-                        paramStr.append("[" + maprepo.get("full_name") + "]");
-                    }
-                    if (mapdata.containsKey("commits")) {
-                        Map<String, Object> mapcommits = gson.fromJson(new ObjectMapper().writeValueAsString(mapdata.get("commits")), map.getClass());
-                        paramStr.append("Commiter" + mapcommits.get("commiter"));
-                    }
+                    mappusher = gson.fromJson((new ObjectMapper()).writeValueAsString(v), map.getClass());
                 } catch (JsonProcessingException e) {
                     e.printStackTrace();
                 }
             }
-        });
-//        map.forEach((k, v) ->{
-//            if(k.equals("pusher")) {
-////                System.out.println(k);
-////                Map<String, Object> mapdata;
-////                try {
-////                    mapdata = gson.fromJson((new ObjectMapper()).writeValueAsString(k), map.getClass());
-////                    if (mapdata.containsKey("repository")) {
-////                        Map<String, Object> maprepo = gson.fromJson(new ObjectMapper().writeValueAsString(mapdata.get("repository")), map.getClass());
-////                        paramStr.append("[" + maprepo.get("full_name") + "]");
-////                    }
-//////                    if(mapdata.containsKey("commits")){
-//////                        Map<String, Object> mapcommits = gson.fromJson(new ObjectMapper().writeValueAsString(mapdata.get("commits")), map.getClass());
-//////                        paramStr.append("Commiter"+mapcommits.get("commiter")+"]");
-//////                    }
-////                } catch (JsonProcessingException e) {
-////                    e.printStackTrace();
-////                }
-//            }
-//        });
-        System.out.println("this is para"+paramStr);
-        return returnString[0];
+            if (k.equals("head_commit")) {
+                try {
+                    mapmessage = gson.fromJson((new ObjectMapper()).writeValueAsString(v), map.getClass());
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (k.equals("repository")) {
+                try {
+                    maprepo = gson.fromJson((new ObjectMapper()).writeValueAsString(v), map.getClass());
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+        paramStr.append(mappusher.get("name") + "在" +
+                        maprepo.get("name") + "项目中提交新代码\n" +
+                        "提交信息：" + mapmessage.get("message")+"\n" +
+                        "提交时间：" + times(mapmessage.get("timestamp")) );
+
+        return paramStr.toString();
 
     }
 
     public static  String times(Object obj) throws ParseException {
         Date date = null;
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
         date = sdf.parse(String.valueOf(obj));
         System.out.println("UTC时间: " + date);
         Calendar calendar = Calendar.getInstance();
@@ -80,17 +71,4 @@ public class MapTextUtils {
         return (new SimpleDateFormat("yyyy-MM-dd")).format(calendar.getTime());
     }
 
-//    public static  String task(Map<String, Object> mapcommits,StringBuffer paramStr,String action,Map<String,Object> map) throws JsonProcessingException, ParseException {
-//        Gson gson = new Gson();
-//
-//        if(mapcommits.containsKey("commiter")){
-//            paramStr.append("操作者：").append(mapcommits.get("commiter"));
-//            if(mapcommits.get("message") == null){
-//                paramStr.append("没有信息");
-//            }else{
-//                paramStr.append(times(mapcommits.get("message")));
-//            }
-//        }
-//        return  paramStr.toString();
-//    }
 }
