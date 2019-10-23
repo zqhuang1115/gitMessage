80ce9c1b5c196ffe04b47ec72a03d3b2c03d88f4
Ziqing Huang <zqhuang@brandeis.edu>
2019-10-23 14:26:58 +0800

    test

--- src/main/java/com/linker/tower/gmessage/controller/GitCallBack.java
+++ src/main/java/com/linker/tower/gmessage/controller/GitCallBack.java
@@ -32,7 +32,9 @@ public class GitCallBack {
     public void callBack(HttpServletRequest request) throws Exception {
 
         String content = IOUtils.toString(request.getInputStream(), request.getCharacterEncoding());
+        log.info("getInputStream: "+ request.getInputStream());
 
+        log.info("requestget: "+ request.getCharacterEncoding());
         log.info("content={}", content);
 
         if (StringUtils.isEmpty(content)) {
@@ -67,8 +69,8 @@ public class GitCallBack {
         TestData testData = new TestData(textContent);
         TestMessage weixinData = new TestMessage(testData);
         weixinData.setMsgType(MsgType.TEXT.getCode());
-        String url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=410c3682-f48f-4930-bc3d-4ffa18048d09";
-        JsonUtils.requestInvoke(url, weixinData);
+        //String url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=410c3682-f48f-4930-bc3d-4ffa18048d09";
+        //JsonUtils.requestInvoke(url, weixinData);
     }
 
 
